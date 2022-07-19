89c1039bab00cf82e5fda07038c2e9f555768b7a
Hangcode99 <kymy8016@gmail.com>
2022-07-19 17:17:40 +0900

    ''

--- common/views.py
+++ common/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def signin(request):
+    return render(request,'common/signin.html')
+
+def signup(request):
+    return render(request,'common/signup.html')
--- insight/views.py
+++ insight/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
 
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    context = {}
+    return render(request, 'insight/home.html', context)
 
 
